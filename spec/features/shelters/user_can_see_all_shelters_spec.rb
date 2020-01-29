c85edd9352a3dedd1a16c5fc0ba1e6f97de334bb
Ezekiel Clark <clarkzeke@gmail.com>
2020-01-28 17:49:14 -0700

    Set up database

--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
-Rspec.describe "shelters index page", type: :feature do
-  it "can see all shelters names" do
-    shelter_1 = Shelter.create(name: "Shelter 1")
+RSpec.describe "shelters index page", type: :feature do
+  context "as a visitor" do
+    it "can see all shelters names" do
+      shelter_1 = Shelter.create(name: "Shelter 1")
 
-    visit "/shelters"
+      visit "/shelters"
 
-    expect(page).to have_content(shelter_1.name)
+      expect(page).to have_content(shelter_1.name)
+    end
   end
 end
